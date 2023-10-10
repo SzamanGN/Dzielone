59c6cb037d9d2d84789bae36b14ce2f8b959bcab
cygus84 <cygus84@gmail.com>
2023-10-10 18:43:27 +0100

    ac
    ac

--- 001-MiniGry/Farma-1-1/src/startAplikacji/StartApp.java
+++ 001-MiniGry/Farma-1-1/src/startAplikacji/StartApp.java
@@ -5,7 +5,7 @@ import okna.OknoGlowne;
 public class StartApp {
 
 	public static void main(String[] args) {
-		// zmiana okna farmy 
+		// zmiana obrazu okna farmy 
 		
 		System.out.println("Farma-1-1");
 		new OknoGlowne();
