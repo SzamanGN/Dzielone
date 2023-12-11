40f9f90cace73d71925590aca152f3de7834cf40
cygus84 <cygus84@gmail.com>
2023-12-11 11:08:37 +0000

    ac
    Dodanie grafiki nowej i mapy

---
+++ 004_Nowe/Moja2DGRA-0-11/src/app/narzedzia/CollisionChecker.java
@@ -0,0 +1,132 @@
+package app.narzedzia;
+
+import app.entity.Entity;
+import app.panele.GamePanel;
+
+public class CollisionChecker {
+	
+	private GamePanel gp;
+
+	public CollisionChecker(GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	public void checkTile(Entity entity) {
+		int entityLeftWorldX = entity.worldX + entity.solidArea.x;
+		int entityRightWolrdX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
+		int etityTopWorldY = entity.worldY + entity.solidArea.y;
+		int etityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
+		
+		int entityLeftCol = entityLeftWorldX / gp.tileSize;
+		int entityRightCol = entityRightWolrdX / gp.tileSize;
+		int etityTopRow = etityTopWorldY / gp.tileSize;
+		int entityBottomRow = etityBottomWorldY / gp.tileSize;
+		
+		int tileNum1, tileNum2;
+		
+		switch(entity.direction) {
+		case "up":
+			etityTopRow = (etityTopWorldY - entity.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[entityLeftCol][etityTopRow];
+			tileNum2 = gp.tileM.mapTileNum[entityRightCol][etityTopRow];
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				entity.collisionOn = true;
+			}
+			break;
+		case "down":
+			entityBottomRow = (etityBottomWorldY + entity.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+			tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				entity.collisionOn = true;
+			}
+			break;
+		case "left":
+			entityLeftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[entityLeftCol][etityTopRow];
+			tileNum2 = gp.tileM.mapTileNum[entityLeftCol][entityBottomRow];
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				entity.collisionOn = true;
+			}
+			break;
+		case "right":
+			entityRightCol = (entityRightWolrdX + entity.speed) / gp.tileSize;
+			tileNum1 = gp.tileM.mapTileNum[entityLeftCol][etityTopRow];
+			tileNum2 = gp.tileM.mapTileNum[entityRightCol][entityBottomRow];
+			if(gp.tileM.tile[tileNum1].collision == true || gp.tileM.tile[tileNum2].collision == true) {
+				entity.collisionOn = true;
+			}
+			break;
+		}
+	}
+	
+	public int checkObject(Entity entity, boolean player) {
+		
+		int index = 999;
+		
+		for(int i = 0;i < gp.obj.length; i++ ) {
+			
+			if(gp.obj[i] != null) {
+				// get entity solid area position
+				entity.solidArea.x = entity.worldX + entity.solidArea.x;
+				entity.solidArea.y = entity.worldY + entity.solidArea.y;
+				// get the objects solid position
+				gp.obj[i].solidArea.x = gp.obj[i].worldX + gp.obj[i].solidArea.x;
+				gp.obj[i].solidArea.y = gp.obj[i].worldY + gp.obj[i].solidArea.y;
+				
+				switch(entity.direction) {
+				case "up":
+					entity.solidArea.y -= entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "down":
+					entity.solidArea.y += entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "left":
+					entity.solidArea.x -= entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				case "right":
+					entity.solidArea.x += entity.speed;
+					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+						if(gp.obj[i].collision == true) {
+							entity.collisionOn = true;
+						}
+						if(player == true) {
+							index = i;
+						}
+					}
+					break;
+				}
+				entity.solidArea.x = entity.solidAreaDefaultX;
+				entity.solidArea.y = entity.solidAreaDefaultY;
+				gp.obj[i].solidArea.x = gp.obj[i].solidAreaDefaultX;
+				gp.obj[i].solidArea.y = gp.obj[i].solidAreaDefaultY;
+			}
+		}
+		
+		return index;
+	}
+}
