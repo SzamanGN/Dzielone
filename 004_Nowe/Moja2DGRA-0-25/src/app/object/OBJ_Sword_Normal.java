4f39cb5769814f8b9c9c07aad9ff129119797214
cygus84 <cygus84@gmail.com>
2023-12-18 14:17:49 +0000

    AC
    mechanizm walki i lvl

---
+++ 004_Nowe/Moja2DGRA-0-25/src/app/object/OBJ_Sword_Normal.java
@@ -0,0 +1,16 @@
+package app.object;
+
+import app.entity.Entity;
+import app.panele.GamePanel;
+
+public class OBJ_Sword_Normal  extends Entity{
+
+	public OBJ_Sword_Normal(GamePanel gp) {
+		super(gp);
+		
+		name = "Normale Sword";
+		down1 = setup("/objects/sword_normal", gp.tileSize, gp.tileSize);
+		attackValue = 1;
+	}
+
+}
